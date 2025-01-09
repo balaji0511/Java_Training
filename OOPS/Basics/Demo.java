a52a65c5b2f8437e173e8b8b7a7b9bcc25d20f82
balaji0511 <balajita2020@gmail.com>
2025-01-09 18:56:39 +0530

    methods concept

---
+++ OOPS/Basics/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String[] args){
+        method1();
+        //method2();
+    }
+    public static void method1(){
+        System.out.println("static method");
+    }
+    public void method2(){
+     System.out.println("non static method");   
+    }
+}
